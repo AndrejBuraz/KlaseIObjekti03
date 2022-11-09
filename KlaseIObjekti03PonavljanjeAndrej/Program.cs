57b1cc1cdfd48c492165cef5fc3f085b8404965f
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2022-11-09 08:59:24 +0100

    Add files via upload

---
+++ KlaseIObjekti03PonavljanjeAndrej/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaseIObjekti03PonavljanjeAndrej
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stablo stablo1 = new Stablo(false);
+            Stablo stablo2 = new Stablo(true);
+            Console.WriteLine("Prvo stablo: \n" + stablo1.ToString());
+            Console.WriteLine("Drugo stablo: \n" + stablo2.ToString());
+            Console.ReadKey();
+        }
+    }
+
+    class Stablo
+    {
+        bool otpadajuListovi;
+        string vrstaStabla;
+
+        public void setOtpadajuListovi(bool otpadajuListovi)
+        {
+            this.otpadajuListovi = otpadajuListovi;
+        }
+        public bool getOtpadajuListovi()
+        {
+            return this.otpadajuListovi;
+        }
+        public Stablo()
+        {
+
+        }
+        public Stablo(bool otpadajuListovi)
+        {
+            this.otpadajuListovi=otpadajuListovi;
+            if(!otpadajuListovi)
+            {
+                this.vrstaStabla = "crnogorično";
+            }
+            else
+            {
+                this.vrstaStabla = "bijelogorično";
+            }
+        }
+        public override string ToString()
+        {
+            string ispis = ("Stablo je: " + this.vrstaStabla);
+            return ispis;
+        }
+    }
+}
